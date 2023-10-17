3d5f76adbbcc4b7a0a97b71810ea6d611e0758fc
Aditya Gupta <adityaguptady@gmail.com>
2023-10-17 13:57:51 +0530

    map examples, string functions

--- MyJavascriptfile.js
+++ MyJavascriptfile.js
@@ -29,9 +29,11 @@ console.log(variable1 === variable2)
 
 let myArray = ["Atif", "SSi", "MERN", 1, 2, true]
 console.log(myArray)
-
+var index = 0;
 myArray.forEach(element =>{
+	console.log(index)
 	console.log(element)
+	index++
 	//all the oprerations related to
 	//the element can be done here
 })
@@ -39,6 +41,26 @@ myArray.forEach(element =>{
 //["Atif", "SSi", "MERN", 1, 2, true]
 for(const [index, value] of Object.entries(myArray))
 {
-	console.log("index: "+index)
-	console.log("value: "+value)
+	console.log("index: "+index+", value: "+value)
 }
+
+
+//Map
+console.log("------------------Map------------------")
+let myMap = new Map()
+myMap.set(1,"Aditya")
+myMap.set(2,"Pragya")
+myMap.set(3,"Salman")
+
+myMap.forEach((value, key)=>
+{
+	console.log("key: "+key+", value: "+value)
+})
+
+//String functions
+console.log("------------------String functions------------------")
+var myStringVariable = "Abcdefghijkl"
+var length = myStringVariable.length
+console.log("Length of the sting is: "+length)
+let myArrayVar = myStringVariable.split("f")
+console.log(myArrayVar[1])
